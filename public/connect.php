4a23105463796d5059a69605d5beedaaa85cae15
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-07-26 16:33:37 +0500

    initial changes

---
+++ public/connect.php
@@ -0,0 +1,109 @@
+<?php
+
+require 'header.php';
+include 'config.php';
+
+function encode_password($plain, $challenge, $secret) {
+  if ((strlen($challenge) % 2) != 0 ||
+    strlen($challenge) == 0)
+    return FALSE;
+
+  $hexchall = hex2bin($challenge);
+  if ($hexchall === FALSE)
+    return FALSE;
+
+  if (strlen($secret) > 0) {
+    $crypt_secret = md5($hexchall . $secret, TRUE);
+    $len_secret = 16;
+  } else {
+    $crypt_secret = $hexchall;
+    $len_secret = strlen($hexchall);
+  }
+
+  /* simulate C style \0 terminated string */
+  $plain .= "\x00";
+  $crypted = '';
+  for ($i = 0; $i < strlen($plain); $i++)
+    $crypted .= $plain[$i] ^ $crypt_secret[$i % $len_secret];
+
+  $extra_bytes = 0;//rand(0, 16);
+  for ($i = 0; $i < $extra_bytes; $i++)
+    $crypted .= chr(rand(0, 255));
+
+  return bin2hex($crypted);
+}
+
+$uam_secret = "secret";
+$redirect_url = $_SERVER['REDIRECT_URL'];
+$mac = $_SESSION["mac"];
+$user_type = $_SESSION["user_type"];
+$last_updated = date("Y-m-d H:i:s");
+
+if ($user_type == "new") {
+  $mobile = $_POST['mobile'];
+  $email = $_POST['email'];
+
+  mysqli_query($con, "
+    CREATE TABLE IF NOT EXISTS `$table_name` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `mobile` varchar(45) NOT NULL,
+    `email` varchar(45) NOT NULL,
+    `mac` varchar(45) NOT NULL,
+    `last_updated` varchar(45) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (mac)
+    )");
+
+  mysqli_query($con,"INSERT INTO `$table_name` (mobile, email, mac, last_updated) VALUES ('$mobile', '$email', '$mac', '$last_updated')");
+}
+
+$username = $mac;
+$password = $mac;
+$uamip = $_SESSION["uamip"];
+$uamport = $_SESSION["uamport"];
+$challenge = $_SESSION["challenge"];
+
+$encoded_password = encode_password($password, $challenge, $uam_secret);
+
+$uam_redirect_url = "http://$uamip:$uamport/logon?" .
+  "username=" . urlencode($username) .
+  "&password=" . urlencode($encoded_password);
+
+header('Location: ' . $uam_redirect_url);
+
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title><?php echo htmlspecialchars($business_name); ?> WiFi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="assets/styles/bulma.min.css" />
+    <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css" />
+    <meta http-equiv="refresh" content="5;url=<?php echo htmlspecialchars($redirect_url); ?>" />
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" href="assets/styles/style.css" />
+</head>
+
+<body>
+<div class="page">
+
+    <div class="head">
+        <br>
+        <figure id="logo">
+            <img src="assets/images/logo.png">
+        </figure>
+    </div>
+
+   <div class="main">
+       <seection class="section">
+           <div id="margin_zero" class="content has-text-centered is-size-6">Thanks, you are now </div>
+           <div id="margin_zero" class="content has-text-centered is-size-6">authorized on WiFi</div>
+       </seection>
+    </div>
+
+</div>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,122 @@
+<?php
+
+require 'header.php';
+include 'config.php';
+
+$_SESSION["mac"] = $_GET["mac"];
+$_SESSION["challenge"] = $_GET["challenge"];
+$_SESSION["uamip"] = $_GET["uamip"];
+$_SESSION["uamport"] = $_GET["uamport"];
+$_SESSION["userurl"] = $_GET["userurl"];
+$_SESSION["user_type"] = "new";
+
+# Checking DB to see if user exists or not.
+$result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[mac]'");
+
+if ($result->num_rows >= 1) {
+  $row = mysqli_fetch_array($result);
+
+  mysqli_close($con);
+
+  $_SESSION["user_type"] = "repeat";
+  header("Location: welcome.php");
+} else {
+  mysqli_close($con);
+}
+
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>
+      <?php echo htmlspecialchars($business_name); ?> WiFi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="stylesheet" href="assets/styles/bulma.min.css"/>
+    <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css"/>
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16"/>
+    <link rel="stylesheet" href="assets/styles/style.css"/>
+</head>
+
+<body>
+
+<?php
+$res = $_GET["res"];
+if ($res === "notyet") {
+?>
+
+<div class="page">
+
+    <div class="head">
+        <br>
+        <figure id="logo">
+            <img src="assets/images/logo.png">
+        </figure>
+    </div>
+
+    <div class="main">
+        <section class="section">
+            <div class="container">
+                <div id="login" class="content is-size-5 has-text-centered has-text-weight-bold">Enter your details
+                </div>
+                <form method="post" action="connect.php">
+
+                    <div class="field">
+                        <div class="control has-icons-left">
+                            <input class="input" type="number" id="form_font" name="mobile" placeholder="Mobile" required>
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <div class="control has-icons-left">
+                            <input class="input" type="email" id="form_font" name="email" placeholder="Email" required>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="columns is-centered is-mobile">
+                        <div class="control">
+                            <label class="checkbox">
+                                <input type="checkbox" required>
+                                I agree to the <a href="policy.php">Terms of Use</a>
+                            </label>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="buttons is-centered">
+                        <button class="button is-link">Connect</button>
+                    </div>
+                </form>
+            </div>
+            <br>
+        </section>
+    </div>
+</div>
+
+<?php
+}
+else if ($res === "success") {
+  $redir = $_SESSION["userurl"];
+  if(isset($redir)) {
+    echo "<head>";
+    echo '<meta http-equiv="refresh" content="3;URL=\'' . $redir . '\'">';
+    echo "</head>";
+  }
+  else {
+    echo "<h2>Log-in successful!</h2>";
+  }
+}
+else if ($res === "failed") {
+  echo "<h2>Whoops, failed to authenticate</h2>";
+}
+else if ($res === "logoff") {
+  echo "<h2>Logging off ...</h2>";
+}
+else {
+  echo "<h2>Oops!, bad 'res' parameter</h2>";
+}
+?>
+
+</body>
+</html>
