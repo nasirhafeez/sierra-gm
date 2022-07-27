91702e7b43a021ffd6efc303cd53e469674792d9
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-07-27 18:01:38 +0500

    formatting

--- public/index.php
+++ public/index.php
@@ -27,7 +27,6 @@ $_SESSION["user_type"] = "new";
 ?>
 <!doctype html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <title>
@@ -38,147 +37,104 @@ $_SESSION["user_type"] = "new";
     <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32"/>
     <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16"/>
     <link rel="stylesheet" href="assets/styles/style.css"/>
+    <style>
+        body {
+            background-image: url("assets/images/bg.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: bottom;
+        }
 
-
-	<style>
-	body{
-	background-image:url("assets/images/bg.jpg");
-	background-repeat: no-repeat;
-    background-size: cover;
-	background-position:bottom;
-
-
-	}
-
-
-	.main-form{
-
-	background-color:#def2f1;
-	padding:50px;
-	margin-left:0;
-	}
-
-
-	</style>
+        .main-form {
+            background-color: #def2f1;
+            padding: 50px;
+            margin-left: 0;
+        }
+    </style>
 </head>
-
-<body >
-
+<body>
 <?php
 
 $res = $_GET["res"];
 $res = "notyet";
 if ($res === "notyet") {
+  ?>
+    <div class="page">
+        <div class="main">
+            <section class="section">
+                <div class="container">
+                    <div class=" main-form">
+                        <div id="login" class="content is-size-5 has-text-centered has-text-weight-bold">Join today.
+                            <br>Its Quick and Easy
+                        </div>
+                        <form method="post" action="connect.php">
 
-?>
+                            <div class="field">
+                                <div class="control has-icons-left">
+                                    <input class="input" type="email" id="form_font" name="email" placeholder="Email"
+                                           required>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control has-icons-left">
+                                    <input class="input" type="number" id="form_font" name="mobile" placeholder="Mobile"
+                                           required>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="columns is-centered is-mobile">
+                                <div class="control">
+                                    <label class="checkbox">
+                                        <input type="checkbox" required>
+                                        Signup for email and text alerts
+                                    </label>
 
-<div class="page">
-
-
-    <div class="main">
-
-        <section class="section">
-            <div class="container">
-
-
-			  <div class=" main-form" >
-
-				      <div id="login" class="content is-size-5 has-text-centered has-text-weight-bold">Join today. <br>Its Quick and Easy
+                                    <label class="checkbox">
+                                        <input type="checkbox" required>
+                                        By Signining in, I agree to <a href="policy.php">Terms of Use</a>
+                                    </label>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="buttons is-centered">
+                                <button class="button is-link">Sign In</button>
+                                <br>
+                            </div>
+                            <div class="buttons is-centered">
+                                <figure id="logoz">
+                                    <h2 style="color:#60c0bc"><strong>sponsered by</strong></h2>
+                                    <br>
+                                    <img width="100px" src="assets/images/logo.png">
+                                </figure>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <form method="post" action="connect.php">
-
-					<div class="field">
-                        <div class="control has-icons-left">
-                            <input class="input" type="email" id="form_font" name="email" placeholder="Email" required>
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <div class="control has-icons-left">
-                            <input class="input" type="number" id="form_font" name="mobile" placeholder="Mobile" required>
-                        </div>
-                    </div>
-
-
-                    <br>
-                    <div class="columns is-centered is-mobile">
-                        <div class="control">
-                            <label class="checkbox">
-                                <input type="checkbox" required>
-                                Signup for email and text alerts
-                            </label>
-
-							  <label class="checkbox">
-                            <input type="checkbox" required>
-                               By Signining in, I agree to <a href="policy.php">Terms of Use</a>
-                            </label>
-
-                        </div>
-
-
-
-
-                    </div>
-                    <br>
-                    <div class="buttons is-centered">
-                        <button class="button is-link">Sign In</button>
-						<br>
-                    </div>
-
-					  <div class="buttons is-centered">
-
-						<figure id="logoz">
-						<h2 style="color:#60c0bc"><strong>sponsered by</strong></h2>
-								<br>
-								<img width="100px" src="assets/images/logo.png">
-						</figure>
-                    </div>
-                </form>
-			  </div>
-            </div>
-            <br>
-        </section>
+                <br>
+            </section>
+        </div>
     </div>
-</div>
+    </div>
+    </div>
 
+  <?php
 
-
-
-
-
-
-
-
-
-
-  </div>
-</div>
-
-<?php
-
-}
-else if ($res === "success") {
+} else if ($res === "success") {
   $redir = $_SESSION["userurl"];
-  if(isset($redir)) {
+  if (isset($redir)) {
     echo "<head>";
     echo '<meta http-equiv="refresh" content="3;URL=\'' . $redir . '\'">';
     echo "</head>";
-  }
-  else {
+  } else {
     echo "<h2>Log-in successful!</h2>";
   }
-}
-else if ($res === "failed") {
+} else if ($res === "failed") {
   echo "<h2>Whoops, failed to authenticate</h2>";
-}
-else if ($res === "logoff") {
+} else if ($res === "logoff") {
   echo "<h2>Logging off ...</h2>";
-}
-else {
+} else {
   echo "<h2>Error: Permission Denied</h2>";
 }
-
 ?>
-
 </body>
 </html>
