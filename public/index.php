1bbf76fa9f51d5520afcdecf6dd8c66cac852273
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-07-28 17:48:58 +0500

    test

--- public/index.php
+++ public/index.php
@@ -11,18 +11,18 @@ $_SESSION["userurl"] = $_GET["userurl"];
 $_SESSION["user_type"] = "new";
 
 # Checking DB to see if user exists or not.
-//$result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[mac]'");
-//
-//if ($result->num_rows >= 1) {
-//  $row = mysqli_fetch_array($result);
-//
-//  mysqli_close($con);
-//
-//  $_SESSION["user_type"] = "repeat";
-//  header("Location: welcome.php");
-//} else {
-//  mysqli_close($con);
-//}
+$result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[mac]'");
+
+if ($result->num_rows >= 1) {
+  $row = mysqli_fetch_array($result);
+
+  mysqli_close($con);
+
+  $_SESSION["user_type"] = "repeat";
+  header("Location: welcome.php");
+} else {
+  mysqli_close($con);
+}
 
 ?>
 <!doctype html>
