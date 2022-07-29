987c36ce702a1d61efffd8fc081359b2c96c4edd
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-07-29 19:51:06 +0500

    debug

--- public/connect.php
+++ public/connect.php
@@ -77,22 +77,22 @@ $uam_redirect_url = "http://$uamip:$uamport/logon?" .
 header('Location: ' . $uam_redirect_url);
 
 ?>
-<!--<!DOCTYPE HTML>-->
-<!--<html>-->
-<!---->
-<!--<head>-->
-<!--    <meta charset="utf-8">-->
-<!--    <title>--><?php //echo htmlspecialchars($business_name); ?><!--</title>-->
-<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />-->
-<!--    <link rel="stylesheet" href="assets/styles/bulma.min.css" />-->
-<!--    <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css" />-->
-<!--    <meta http-equiv="refresh" content="5;url=--><?php //echo htmlspecialchars($redirect_url); ?><!--" />-->
-<!--    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />-->
-<!--    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />-->
-<!--    <link rel="stylesheet" href="assets/styles/style.css" />-->
-<!--</head>-->
-<!---->
-<!--<body>-->
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title><?php echo htmlspecialchars($business_name); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="assets/styles/bulma.min.css" />
+    <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css" />
+    <meta http-equiv="refresh" content="5;url=<?php echo htmlspecialchars($redirect_url); ?>" />
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" href="assets/styles/style.css" />
+</head>
+
+<body>
 <!--<div class="page">-->
 <!---->
 <!--    <div class="head">-->
@@ -110,5 +110,18 @@ header('Location: ' . $uam_redirect_url);
 <!--    </div>-->
 <!---->
 <!--</div>-->
-<!--</body>-->
-<!--</html>-->
+
+
+<div class="page">
+    <img src="assets/images/new-bg.jpg" alt="" class="img-responsive hidden-xs">
+    <div class=" main-form">
+        <div id="login" class="content is-size-5 has-text-centered has-text-weight-bold">
+            <span class="quick">Thanks, you are now </span>
+            <span class="quick">authorized on WiFi</span>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
