05009ead5d20e370eb0f71bf3a7e042a0c2d18de
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-07-29 19:54:11 +0500

    debug

--- public/connect.php
+++ public/connect.php
@@ -79,7 +79,6 @@ header('Location: ' . $uam_redirect_url);
 ?>
 <!DOCTYPE HTML>
 <html>
-
 <head>
     <meta charset="utf-8">
     <title><?php echo htmlspecialchars($business_name); ?></title>
@@ -91,27 +90,7 @@ header('Location: ' . $uam_redirect_url);
     <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
     <link rel="stylesheet" href="assets/styles/style.css" />
 </head>
-
 <body>
-<!--<div class="page">-->
-<!---->
-<!--    <div class="head">-->
-<!--        <br>-->
-<!--        <figure id="logo">-->
-<!--            <img src="assets/images/logo.png">-->
-<!--        </figure>-->
-<!--    </div>-->
-<!---->
-<!--   <div class="main">-->
-<!--       <seection class="section">-->
-<!--           <div id="margin_zero" class="content has-text-centered is-size-6">Thanks, you are now </div>-->
-<!--           <div id="margin_zero" class="content has-text-centered is-size-6">authorized on WiFi</div>-->
-<!--       </seection>-->
-<!--    </div>-->
-<!---->
-<!--</div>-->
-
-
 <div class="page">
     <img src="assets/images/new-bg.jpg" alt="" class="img-responsive hidden-xs">
     <div class=" main-form">
@@ -121,7 +100,5 @@ header('Location: ' . $uam_redirect_url);
         </div>
     </div>
 </div>
-
-
 </body>
 </html>
